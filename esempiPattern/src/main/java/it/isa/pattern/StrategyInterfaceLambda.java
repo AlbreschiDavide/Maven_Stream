133fa6b04fbbbcdd1509b0d503654b2ee933168d
AlbreschiDavide <albreschi1997@gmail.com>
2022-05-03 16:45:55 +0200

    Strategy Interface con funzioni lambda

--- esempiPattern/src/main/java/it/isa/pattern/StrategyInterfaceLambda.java
+++ esempiPattern/src/main/java/it/isa/pattern/StrategyInterfaceLambda.java
@@ -5,12 +5,19 @@ interface StrategyIntLamda{
     public String doOp(String s);
 }
 public class StrategyInterfaceLambda{
+    public static StrategyIntLamda Convert(){
+        return s -> s.toUpperCase(); 
+    }
+    
     public static void esegui() {
         System.out.println("Strategy pattern con lamda");
         String s= "pRoVa";
 
         StrategyIntLamda siLowerLamda = stringa -> stringa.toLowerCase();
         System.out.println(siLowerLamda.doOp(s));
+        
+        StrategyIntLamda siUpperLamda=Convert();
+        siUpperLamda.doOp(s);
     }
 }
 
